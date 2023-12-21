039d6063adb5c480f9485852205a7af234a95ed8
Chris 'KageKirin' Helmich <kagekirin@gmail.com>
2023-12-21 12:17:13 +0900

    file: add extras.sh

---
+++ linux/ubuntu/scripts/extras.sh
@@ -0,0 +1,23 @@
+#!/bin/bash -e
+################################################################################
+##  File:  extras.sh
+##  Desc:  Installs a couple more tools via APT
+################################################################################
+
+# Install Extras
+printf "\n\t🐋 Installing Extras 🐋\t\n"
+apt-get update -y -qq
+apt-get install -y \
+    fd-find \
+    ripgrep \
+    curl \
+    wget \
+    sed \
+    moreutils \
+    jq \
+    zsh \
+    cmake \
+    apt-utils \
+    snap \
+
+printf "\n\t🐋 Installed Extras 🐋\t\n"
